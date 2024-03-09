7e1e5953a20b4370eee59c5cca7eecc014dbe860
singhsatyendra8052 <singhsatyendra8052@gmail.com>
2024-03-09 15:21:56 +0530

    routes
    routing

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+import investor from "./routes/investor.js";
+import requestor from "./routes/requestor.js";
+
+//routes from investor and requestor
+app.use("/requestor", requestor);
+app.use("/investor", investor);
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
